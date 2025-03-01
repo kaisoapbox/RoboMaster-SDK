b0cd8e0f1c64fe64c2d6ba2d9571f7e0bac7929e
Ryan Kaien Nah <ryan.nah@tribex.co>
2025-02-28 18:42:44 -0800

    add pybind11 submodule, bump version

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 """
 RoboMaster SDK is based on the Python language and is suitable for the Python SDK software library
  of the RoboMater series.
@@ -9,9 +8,11 @@ and can be quickly used to facilitate learning and teaching. Based on each API i
 there are code examples, you can refer to our developer documentation robomaster-dev.readthedocs.io.
 """
 
-from setuptools import setup, find_packages
 import os.path
 import sys
+
+from setuptools import find_packages, setup
+
 if sys.version_info < (3, 6, 5):
     sys.exit("RoboMaster SDK requires Python 3.6.5 or later")
 
@@ -19,37 +20,36 @@ curr = os.path.abspath(os.path.dirname(__file__))
 
 
 def fetch_version():
-    with open(os.path.join(curr, 'src', 'robomaster', 'version.py')) as f:
+    with open(os.path.join(curr, "src", "robomaster", "version.py")) as f:
         ns = {}
         exec(f.read(), ns)
         return ns
 
 
 version_data = fetch_version()
-version = version_data['__version__']
+version = version_data["__version__"]
 
 
 setup(
-    name='robomaster',
+    name="robomaster",
     version=version,
     description="RoboMaster Python SDK",
     long_description=__doc__,
     long_description_content_type="text/markdown",
-    author='EDU SDK TEAM',
-    license='Apache License, Version 2.0',
+    author="EDU SDK TEAM",
+    license="Apache License, Version 2.0",
     zip_safe=True,
-    keywords='dji robomaster sdk robot drone'.split(),
+    keywords="dji robomaster sdk robot drone".split(),
     url="http://www.robomaster.com",
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    package_data={
-      'robomaster': ['LICENSE.txt', 'README.md']
-    },
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    package_data={"robomaster": ["LICENSE.txt", "README.md"]},
     install_requires=[
-        'numpy >= 1.18',
-        'opencv-python >= 4.2',
-        'netaddr >= 0.8',
-        'netifaces >= 0.10',
-        'myqr >= 2.3'
-    ]
+        "numpy >= 1.18",
+        "opencv-python >= 4.2",
+        "netaddr >= 0.8",
+        "netifaces >= 0.10",
+        "myqr >= 2.3",
+        "audioop-lts >= 0.2.1; python_version>='3.13'",
+    ],
 )
